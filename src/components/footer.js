4470dba2e99632bd5766099b402262c656116c09
aoruize <aoruize.matthew@gmail.com>
2021-07-22 06:32:02 -0400

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,12 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
-        <span className="icon -love">
-          <RiHeart2Line />
-        </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        <Link to="/">Contact Us</Link>{" "}|{" "}
+        <Link to="/">Accessibility</Link>{" "}|{" "}
+        <Link to="/">Privacy</Link>{" "}|{" "}
+        <Link to="/">Site Map</Link>{" "}|{" "}
+        <Link to="/">© Queen's Printer for Ontario</Link>{" "}|{" "}
+        <Link to="/">Terms of Use</Link>{" "}|{" "}
       </p>
     </div>
   </footer>
